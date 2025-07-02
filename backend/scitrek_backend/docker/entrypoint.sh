85b2dd5009a18add74e6a1ea02ce58c1c5c5e952
rsolorzano-ucsb <teamrafu@gmail.com>
2025-07-02 15:17:16 -0700

    Dockerize app.

--- backend/scitrek_backend/docker/entrypoint.sh
+++ backend/scitrek_backend/docker/entrypoint.sh
@@ -7,14 +7,7 @@ until nc -z "$DATABASE_HOST" "$DATABASE_PORT"; do
   sleep 1
 done
 
-# 1. migrate
 python manage.py migrate --noinput
-
-# 2. seed
 python manage.py seed_inbox
+exec gunicorn scitrek_backend.wsgi:application --bind 0.0.0.0:8000 --workers 3 --log-level info
 
-# 3. start Gunicorn
-exec gunicorn scitrek_backend.wsgi:application \
-     --bind 0.0.0.0:8000 \
-     --workers 3 \
-     --log-level info
