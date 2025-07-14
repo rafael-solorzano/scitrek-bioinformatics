6baac0afc61a2ca70e5e1e3fc2123b8a13e68c73
rsolorzano-ucsb <teamrafu@gmail.com>
2025-07-14 10:08:45 -0700

    Update celery broker in base settings.

--- backend/scitrek_backend/scitrek_backend/settings/base.py
+++ backend/scitrek_backend/scitrek_backend/settings/base.py
@@ -140,7 +140,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Broker & result backend (e.g. Redis)
-CELERY_BROKER_URL    = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 
 # Optional Celery settings:
